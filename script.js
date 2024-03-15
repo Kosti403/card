91da573ef8e7a2dde00531000c0bde8d2ecf4a30
Kosti403 <anim.myay@gmail.com>
2024-03-15 17:28:59 +0500

    finish

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
-const CLIENT_ID = "kfchB4vUrI8Lg69oT6lfTesriOq3lqCVN0i5fpYoFV0";
+const CLIENT_ID = "mn1GmQR_maeKWSuHnnBBDSd_G0NEgmgcdsU6XIIss5w";
 const slider = document.getElementById("slider");
 
-let slides;
 let state = [];
 let currentSlide;
 
 const fetchPhoto = async () => {
-  const url = `https://api.unsplash.com/photos/random?client_id=${CLIENT_ID}&count=4&query=food`;
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
+  try {
+    const url = `https://api.unsplash.com/photos/random?client_id=${CLIENT_ID}&count=4`;
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      if (data.length) {
+        state = data;
+        currentSlide = data[0].id;
+        setPhotos();
+      }
+    } else {
+      console.error("Error or rate limit exceeded");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const renderItem = () => {
+  return state
+    .map(({ urls: { regular }, user: { name }, id }) => {
+      const isActive = currentSlide === id ? "active" : "";
+      return `<div class="slide ${isActive}" data-id=${id} style="background-image: url(${regular})">
+    <div class="slide-text">
+    <span> photo by </span>
+    ${name}
+    </div>
+  </div>`;
+    })
+    .join("");
+};
+
+const handleClick = ({ currentTarget }) => {
+  const slides = document.querySelectorAll(".slide");
+  const { id } = currentTarget.dataset;
+  if (id === currentSlide) return;
+  slides.forEach((slide) => slide.classList.remove("active"));
+  currentTarget.classList.add("active");
+  currentSlide = id;
+};
+
+const setPhotos = () => {
+  slider.innerHTML = renderItem();
+  const slides = document.querySelectorAll(".slide");
+  slides.forEach((slide) => {
+    slide.addEventListener("click", handleClick);
+  });
 };
 
 fetchPhoto();
